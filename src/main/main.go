c6eeb2275972bb7b5880db250bef292193d186f9
shu <verybluebot@gmail.com>
2017-01-23 22:02:37 +0200

    :sparkles: part 2: parms and json response

--- src/main/main.go
+++ src/main/main.go
@@ -8,8 +8,31 @@ import(
     "github.com/labstack/echo"
 )
 
+
 func yallo(c echo.Context) error {
     return c.String(http.StatusOK, "yallo from the web side!")
+}
+
+func getCats(c echo.Context) error {
+    catName := c.QueryParam("name")
+    catType := c.QueryParam("type")
+
+    dataType := c.Param("data")
+
+    if dataType == "string" {
+        return c.String(http.StatusOK, fmt.Sprintf("your cat name is: %s\nand his type is: %s\n", catName, catType))
+    }
+
+    if dataType == "json" {
+        return c.JSON(http.StatusOK, map[string]string{
+            "name": catName,
+            "type": catType,
+        })
+    }
+
+    return c.JSON(http.StatusBadRequest, map[string]string{
+        "error": "you need to lets us know if you want json or string data",
+    })
 }
 
 func main() {
@@ -18,6 +41,7 @@ func main() {
     e := echo.New()
 
     e.GET("/", yallo)
+    e.GET("/cats/:id", getCats)
 
     e.Start(":8000")
 }
