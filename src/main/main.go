18ea63909dd177bc88e1a6960d9deb4dab66dae5
shu <verybluebot@gmail.com>
2017-01-29 14:17:21 +0200

    :sparkles: 3 ways to parse json from http request body

--- src/main/main.go
+++ src/main/main.go
@@ -4,10 +4,27 @@ package main
 import(
     "fmt"
     "net/http"
+    "io/ioutil"
+    "log"
+    "encoding/json"
 
     "github.com/labstack/echo"
 )
 
+type Cat struct {
+    Name        string    `json:"name"`
+    Type        string    `json:"type"`
+}
+
+type Dog struct {
+    Name        string    `json:"name"`
+    Type        string    `json:"type"`
+}
+
+type Hamster struct {
+    Name        string    `json:"name"`
+    Type        string    `json:"type"`
+}
 
 func yallo(c echo.Context) error {
     return c.String(http.StatusOK, "yallo from the web side!")
@@ -35,6 +52,55 @@ func getCats(c echo.Context) error {
     })
 }
 
+func addCat(c echo.Context) error {
+    cat := Cat{}
+
+    defer c.Request().Body.Close()
+
+    b, err := ioutil.ReadAll(c.Request().Body)
+    if err != nil {
+        log.Printf("Failed reading the request body for addCats: %s\n", err)
+        return c.String(http.StatusInternalServerError, "")
+    }
+
+    err = json.Unmarshal(b, &cat)
+    if err != nil {
+        log.Printf("Failed unmarshaling in addCats: %s\n", err)
+        return c.String(http.StatusInternalServerError, "")
+    }
+
+    log.Printf("this is your cat: %#v\n", cat)
+    return c.String(http.StatusOK, "we got your cat!")
+}
+
+func addDog(c echo.Context) error {
+    dog := Dog{}
+
+    defer c.Request().Body.Close()
+
+    err := json.NewDecoder(c.Request().Body).Decode(&dog)
+    if err != nil {
+        log.Printf("Failed processing addDog request: %s\n", err)
+        return echo.NewHTTPError(http.StatusInternalServerError)
+    }
+
+    log.Printf("this is your dog: %#v", dog)
+    return c.String(http.StatusOK, "we got your dog!")
+}
+
+func addHamster(c echo.Context) error {
+    hamster := Hamster{}
+
+    err := c.Bind(&hamster)
+    if err != nil {
+        log.Printf("Failed processing addHamster request: %s\n", err)
+        return echo.NewHTTPError(http.StatusInternalServerError)
+    }
+
+    log.Printf("this is your hamster: %#v", hamster)
+    return c.String(http.StatusOK, "we got your hamster!")
+}
+
 func main() {
     fmt.Println("Welcome to the server")
 
@@ -42,6 +108,10 @@ func main() {
 
     e.GET("/", yallo)
     e.GET("/cats/:id", getCats)
+
+    e.POST("/cats", addCat)
+    e.POST("/dogs", addDog)
+    e.POST("/hamsters", addHamster)
 
     e.Start(":8000")
 }
