048895312e27294cd918568ae7d42eaafe91f089
shu <verybluebot@gmail.com>
2017-04-25 09:09:13 +0300

    part 6 - custom middlewares

--- src/main/main.go
+++ src/main/main.go
@@ -106,10 +106,22 @@ func mainAdmin(c echo.Context) error {
     return c.String(http.StatusOK, "horay you are on the secret amdin main page!")
 }
 
+//////////////////// middlewares ///////////////////////
+func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
+    return func(c echo.Context) error {
+        c.Response().Header().Set(echo.HeaderServer, "BlueBot/1.0")
+        c.Response().Header().Set("notReallyHeader", "thisHaveNoMeaning")
+
+        return next(c)
+    }
+}
+
 func main() {
     fmt.Println("Welcome to the server")
 
     e := echo.New()
+
+    e.Use(ServerHeader)
 
     g := e.Group("/admin")
 
